e420b3258c768a9d7ce26e6887e222202f8d1c6c
g000001 <chiba.masaomi@gmail.com>
2024-08-09 14:55:32 +0900

    export: s-expression-comments

--- package.lisp
+++ package.lisp
@@ -4,7 +4,8 @@
 
 (defpackage "https://github.com/g000001/srfi-62"
   (:export :enable-s-expression-comments
-           :disable-s-expression-comments))
+           :disable-s-expression-comments
+           :s-expression-comments))
 
 (defpackage "https://github.com/g000001/srfi-62#internals"
   (:use "https://github.com/g000001/srfi-62"
